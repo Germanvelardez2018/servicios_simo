58b47f79a353e39b6d8cc51137a7c68ccb077ae9
German <germanvelardez16@gmail.com>
2023-09-04 18:15:56 -0300

    [servicios/db] Se agrego el servicios de base de datos mongo. El mismo se configura mediante parametros o utilizando variables de entorno

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-def App(config=None):
-    pass
+import servicios.db as db
+import time
+import pprint
 
+
+
+def get_date():
+    #Obtengo la fecha de hoy y busco datos relacionados
+        # Obtener la estructura de tiempo actual
+    now = time.localtime()
+    # Obtener el año, mes y día
+    y = now.tm_year
+    m = now.tm_mon if now.tm_mon > 9 else f"0{now.tm_mon}"
+    d = now.tm_mday if now.tm_mday > 9 else f"0{now.tm_mday}"
+    return f"{y}{m}{d}"
+
+
+class App:
+    def __init__(self,app_name="App",**config) -> None:
+        self.name = app_name
+        self.config = config
+
+
+    def get_data_by_date(self,date=None):
+        if date is None:
+            date = get_date() 
+        print(f"find data from {date}")
+        collection = db.get_collection(date)
+        return collection
+        
+
+    def find_data(self,date=None,**filters):
+        collection = self.get_data_by_date(date)
+        print(f"filters:{filters}")
+        return collection.find(filters)
+     
+        
+        
+     
 
 
 
@@ -8,5 +44,11 @@ def App(config=None):
 
 
 if __name__ == "__main__":
-    config = {}
-    App(config)
+    print("Iniciando el programa")
+    print(f"date today is {get_date()}")
+    app = App("simo interface")
+
+    data = app.find_data("20230824")
+    for element in data:
+        pprint.pprint(element)
+   
--- servicios/db.py
+++ servicios/db.py
@@ -10,21 +10,33 @@ from dotenv import load_dotenv
 # Carga las variables de entorno desde el archivo .env
 load_dotenv()
 
+
+
+
+
+
+
+
 # Accede a las variables de entorno cargadas
 DB_URL = os.getenv("MONGO_URL")
 DB_DEFAULT_NAME = os.getenv("DB_DEFAULT_NAME")
-COLLECTION_DEFUALT_NAME = os.getenv("COLLECTION_DEFUALT_NAME")
 
 
 
 
-client =pymongo.MongoClient(DB_URL) 
+def get_client(url=DB_URL):
+    """ Devuelve el cliente Mongo"""
+    return pymongo.MongoClient(url)
 
 
-
-def get_db(db_name=DB_DEFAULT_NAME):
-    return client[db_name]
+def get_db(db_name=DB_DEFAULT_NAME,db_url = DB_URL):
+    """Devuelve la base de datos"""
+    client = get_client(db_url)
+    return  client[db_name]
+   
  
-def get_collection(collection_name=COLLECTION_DEFUALT_NAME,db_name=DB_DEFAULT_NAME):
-    return (client[db_name])[collection_name] 
+def get_collection(collection_name,db_name=DB_DEFAULT_NAME,db_url=DB_URL):
+    """Devuelve la coleccion de datos"""
+    db = get_db(db_name)
+    return  db[collection_name]
 
